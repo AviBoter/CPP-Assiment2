ba90f1521bf5dfc8d9790fe43649689bbca96239
AviBoter <maccavi3@gmail.com>
2020-04-13 16:28:42 +0300

    sec

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -7,14 +7,39 @@
 
 using namespace std;
 
-namespace Familia{
-    FamilyTree::FamilyTree(){
-      head=new node;
+namespace MyTree{
+
+    FamilyTree::Tree(string name)
+    {
+      root=new node();
+      setname(name);
     }
-    FamilyTree::~FamilyTree(){
-      FamilyTree::delete(&head);
+    FamilyTree::~Tree()
+    {
+      if(this->left!=null){
+        delete(this->left);
+      }
+      if(this->right!=null){
+        delete(this->right);
+      }
+        delete(this);
     }
  
-    FamilyTree::addFather()
-
+    FamilyTree::addFather(string name)
+    {
+      setright(name,this->height+1);
+    }
+    FamilyTree::addMoter(string name)
+    {
+       setleft(name,this->height+1);
+    }
+    FamilyTree::relation(string name,node* leaf)
+    {
+      cout<"de"<endl;
+      return 
+    }
+    FamilyTree::find(string name,node* leaf)
+    {
+      cout<"dam"<endl;
+    }
 };
--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -1,32 +1,42 @@
 #ifndef FamilyTree
 #define FamilyTree
 
-namespace Familia{
+namespace MyTree{
 
 struct node
 {
+  private:
   int height;  
   string name;
-  node *left;
-  node *right;
+  node* left;
+  node* right;
+
+public:
+ void setname(string name);
+ string getname();
+ void setheight(int height);
+ int getheight();
+ void setleft(string name,int height);
+ node* getleft();
+ void setright(string name,int height);
+ node* getright();
 }; //node
 
 class FamilyTree
 {
     public:
-        FamilyTree();
-        ~FamilyTree();
+        Tree();
+        ~Tree();
  
     private:
-        void destroy_tree(node *leaf);
         void addFather(string name, node *leaf);
         void addMother(string name, node *leaf);
-        node *find(int key, node *leaf);
-        node *relation(string name, node *leaf);
+        string find(int key, node *leaf);
+        string relation(string name, node *leaf);
         void delete(string name, node *leaf);
         void display(node *leaf);
 
-        node *head;
+        node* root;
 
  }; //FamilyTree
 }; //Familia
